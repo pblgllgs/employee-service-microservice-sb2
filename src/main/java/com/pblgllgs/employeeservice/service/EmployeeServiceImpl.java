b19fb7fc96e5fc692aa040e33a9595597cec118a
pbl.gllgs <pbl.gllgs@gmail.com>
2023-08-09 22:35:53 -0400

    resiliance4j, circuitbreaker and retry patters

--- src/main/java/com/pblgllgs/employeeservice/service/EmployeeServiceImpl.java
+++ src/main/java/com/pblgllgs/employeeservice/service/EmployeeServiceImpl.java
@@ -2,12 +2,17 @@ package com.pblgllgs.employeeservice.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pblgllgs.employeeservice.client.DepartmentClient;
+import com.pblgllgs.employeeservice.dto.DepartmentDto;
 import com.pblgllgs.employeeservice.dto.EmployeeDto;
 import com.pblgllgs.employeeservice.entity.ApiResponseDto;
 import com.pblgllgs.employeeservice.entity.Employee;
 import com.pblgllgs.employeeservice.exception.ResourceNotFoundException;
 import com.pblgllgs.employeeservice.repository.EmployeeRepository;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,6 +22,8 @@ public class EmployeeServiceImpl implements EmployeeService {
     //    private final RestTemplate restTemplate;
 //    private final WebClient webClient;
     private final DepartmentClient departmentClient;
+
+    private static  final Logger LOGGER = LoggerFactory.getLogger(EmployeeServiceImpl.class);
 
 
     @Override
@@ -28,7 +35,10 @@ public class EmployeeServiceImpl implements EmployeeService {
     }
 
     @Override
+//    @CircuitBreaker(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
+    @Retry( name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
     public ApiResponseDto findEmployeeById(Long employeeId) {
+        LOGGER.info("inside getEmployeeById method");
         ObjectMapper objectMapper = new ObjectMapper();
         Employee employeeDb =
                 employeeRepository
@@ -52,6 +62,25 @@ public class EmployeeServiceImpl implements EmployeeService {
         return new ApiResponseDto(
                 objectMapper.convertValue(employeeDb, EmployeeDto.class),
                 departmentClient.findDepartmentById(employeeDb.getDepartmentCode()).getBody()
+        );
+    }
+
+    public ApiResponseDto getDefaultDepartment(Long employeeId, Exception ex) {
+        LOGGER.info("inside getDefaultDepartment method");
+        ObjectMapper objectMapper = new ObjectMapper();
+        Employee employeeDb =
+                employeeRepository
+                        .findById(employeeId)
+                        .orElseThrow(() -> new ResourceNotFoundException("Employee", "id", employeeId));
+        DepartmentDto departmentDto = new DepartmentDto();
+        departmentDto.setId(2L);
+        departmentDto.setDepartmentName("DEV");
+        departmentDto.setDepartmentCode("002-DEV");
+        departmentDto.setDepartmentDescription("DEV");
+
+        return new ApiResponseDto(
+                objectMapper.convertValue(employeeDb, EmployeeDto.class),
+                departmentDto
         );
     }
 }
